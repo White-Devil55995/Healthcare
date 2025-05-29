0a010355f3e6442087d69af7ab4e98f8263b9ad7
White-Devil55995 <sivaprassd55995@gmail.com>
2025-05-29 12:16:01 +0530

    chanmged image to import

--- src/Components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
+++ src/Components/DashboardMainContent/DashboardOverview/AnatomySection.jsx
@@ -1,11 +1,12 @@
 import 'remixicon/fonts/remixicon.css';
 import "./AnatomySection.css"
+import body from "../../../assets/humanbody.png";
 const AntomySection = () => {
     return(
         <>
           <div className="humanbody">
             <div className="body">
-                <img src="./src/assets/humanbody.png" alt="human-body"  className='human-body'/>
+                <img src={body} alt="human-body"  className='human-body'/>
                 <p className='healthy-heart'><i className="ri-heart-fill heart-icon"></i> Healthly Heart</p>
                 <p className='search-block1'><i className="ri-search-eye-line search-icon1" ></i></p>
                 <p className='leg-status'><img src="./src/assets/leg.png" alt="leg-pic" className='leg-pic' /> Healthly leg </p>
